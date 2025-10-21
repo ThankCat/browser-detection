54e44b7170f9137e6668b7612dfa9b50106261a6
ThankCat <chenhao852@icloud.com>
2025-10-21 11:12:06 +0800

    Implement structural updates and optimizations across multiple modules

---
+++ app/bot/page.tsx
@@ -0,0 +1,10 @@
+export default function BotPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="flex flex-col gap-4 text-left">
+        <h1 className="text-5xl font-bold">Bot Detection</h1>
+        <p className="text-2xl font-bold">机器人检测</p>
+      </div>
+    </div>
+  );
+}
---
+++ app/menu/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+import * as React from "react";
+import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+export default function NavigationMenuPage() {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList className="flex-wrap">
+        <NavigationMenuItem>
+          <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+            <Link href="/home">Home</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+            <Link href="/bot">Bot Detection</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
